6a641ff2c801d0b505f9480bde7f776341a3a9f8
hbeteta1323 <hbeteta1323@yahoo.com>
2013-09-14 00:10:44 -0600

    Inicializar proyecto de lista simple

---
+++ src/com/example/listasimple/DetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.listasimple;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DetailActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_detail);
+		TextView textNombre=(TextView)findViewById(R.id.textNombre);
+		Intent intent = getIntent();
+		textNombre.setText(intent.getStringExtra("nombre"));
+	}
+}
